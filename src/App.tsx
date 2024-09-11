91652bf573af2e046bfffae8d5a9ad88754fd509
hamzarafi <hamzarafi62@gmail.com>
2024-09-11 01:45:32 -0400

    feat: adding materialUI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import ComicPage from "./pages/comicPage/ComicPage";
 import RandomComicPage from "./pages/randomComicPage/RandomComicPage";
 import ErrorPage from "./pages/errorPage/ErrorPage";
+import styled from "styled-components";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#056e83",
+    },
+    secondary: {
+      main: "#0ea4c6",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/random" element={<RandomComicPage />} />
-          <Route path="/:comicNumber" element={<ComicPage />} />
-          <Route path="/" element={<ComicPage />} />
+    <MainContainer className="App">
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/random" element={<RandomComicPage />} />
+            <Route path="/:comicNumber" element={<ComicPage />} />
+            <Route path="/" element={<ComicPage />} />
 
-          <Route path="/error" element={<ErrorPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+            <Route path="/error" element={<ErrorPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </MainContainer>
   );
 }
 
