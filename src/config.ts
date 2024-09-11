2f0765ed7c9c913a5a787511f56b44dabc0be241
hamzarafi <hamzarafi62@gmail.com>
2024-09-10 23:39:09 -0400

    chore: pull backend url from env

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const backendURL = "https://comicstrip-be-1.onrender.com";
+// export const backendURL = "https://comicstrip-be-1.onrender.com";
+export const backendURL = process.env.COMIC_API_URL || "http://localhost:8000";
